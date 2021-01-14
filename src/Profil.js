97ad8a51bcae7c26165453d82fbc57006e6db0eb
Mustafa <startmk2020@gmail.com>
2021-01-14 19:49:00 +0100

    Création du component profil et importation dans app

---
+++ src/Profil.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+
+class Profil extends Component {
+
+    render(){
+
+        return(
+            <div className="Profil">
+
+               <h1>Prenom:</h1>
+               <h2>Nom:</h2>
+               <p>Description:</p>
+
+            </div>
+        )
+    }
+}
+
+export default Profil;
